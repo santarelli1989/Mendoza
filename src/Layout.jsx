c05b6e1b42d183dfbe9ea08126170bd7cad53767
Oscar <oal0425@gmail.com>
2025-02-27 12:15:28 -0300

    Updated Layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,16 +1,9 @@
-<<<<<<< Updated upstream
-import React from 'react'
-import Header from './componentes/Header/Header'
-import Footer from './componentes/Footer/Footer'
-import Hero from './componentes/Hero/Hero'
-import Carousel from './componentes/Carousel/Carousel'
-import Card from './componentes/Card/Card'
-
-=======
 import React from "react";
 import Header from "./componentes/Header/Header";
 import Footer from "./componentes/Footer/Footer";
->>>>>>> Stashed changes
+import Hero from "./componentes/Hero/Hero";
+import Carousel from "./componentes/Carousel/Carousel";
+import Card from "./componentes/Card/Card";
 
 const Layout = ({ children }) => {
   return (
@@ -24,21 +17,4 @@ const Layout = ({ children }) => {
   );
 };
 
-<<<<<<< Updated upstream
-const Layout = () => {
-    return (
-      <div className=''>
-        <Header />
-        <Hero/>
-        <Carousel/>
-        <Card/>
-        <Footer />
-      </div>
-      
-    )
-  }
-  
-  export default Layout;
-=======
 export default Layout;
->>>>>>> Stashed changes
